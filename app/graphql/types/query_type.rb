b85b977a115f129877d6d93e6759c20a9c83b5ec
Orban Botond <orbanbotond@toptal.com>
2020-03-22 20:44:47 +0200

    Filter is added.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,9 +3,5 @@ module Types
     field :all_links, [LinkType], null: false,
           description: "All the links in the System",
           resolver: Resolvers::LinksSearch
-
-    # def all_links
-    #   Link.all
-    # end
   end
 end
